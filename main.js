7b76bfc0e601b37883eb5a3c7b87bb6628da5b5a
gabriel <gabriel199777@outlook.com>
2024-09-13 12:03:22 -0300

    ProyectoRickAndMorty

---
+++ main.js
@@ -0,0 +1,114 @@
+const urlEpisodes = `https://rickandmortyapi.com/api/episode`;
+const urlCharacters = `https://rickandmortyapi.com/api/character`;
+const urlLocaciones = `https://rickandmortyapi.com/api/location`;
+
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+            episodes: [],
+            characters: [],
+            locaciones: [],
+            textSearch: "",
+            statusFilter: [],
+            isCharacters: true,
+            currentPage: 1,
+            rowsPerPage: 20,
+            totalPages: 1,
+            loading: true
+        }
+    },
+    created() {
+        this.fetchAllCharacters();
+        this.fetchAllEpisodes();
+        this.fetchAllLocaciones();
+    },
+    methods: {
+        fetchallPage(url) {
+            let allResults = [];
+            function fetchPage(url) {
+                return fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        allResults = allResults.concat(data.results);
+                        if (data.info.next) {
+                            return fetchPage(data.info.next);
+                        } else {
+                            return { results: allResults, totalPages: data.info.pages };
+                        }
+                    })
+            }
+            return fetchPage(url);
+        },
+        fetchAllCharacters() {
+            this.loading = true;
+            this.fetchallPage(urlCharacters)
+                .then(data => {
+                    this.characters = data.results;
+                    this.totalPages = data.totalPages;
+                    this.loading = false;
+                })
+                .catch(error => {
+                    console.error("Error fetching characters:", error);
+                    this.loading = false;
+                });
+        },
+        fetchAllEpisodes() {
+            this.fetchallPage(urlEpisodes)
+                .then(data => {
+                    this.episodes = data.results;
+                })
+                .catch(error => {
+                    console.error("Error fetching episodes:", error);
+                });
+        },
+        fetchAllLocaciones() {
+            this.fetchallPage(urlLocaciones)
+                .then(data => {
+                    this.locaciones = data.results;
+                })
+                .catch(error => {
+                    console.error("Error fetching locaciones:", error);
+                });
+        },
+
+        // Paginación, que la pagina acual sea la 1 por defecto, y que la ultima que no se pase sino que sea la ultima
+        goToPage(page) {
+            if (page >= 1 && page <= this.totalPages) {
+                this.currentPage = page;
+            }
+        },
+        nextPage() {
+            this.goToPage(this.currentPage + 1);
+        },
+        prevPage() {
+            this.goToPage(this.currentPage - 1);
+        }
+    },
+    computed: {
+        filterData() {
+            const data = this.characters;
+            const filteredData = data.filter(item => {
+                const nameMatch = item.name.toLowerCase().includes(this.textSearch.toLowerCase());
+                const statusMatch = this.statusFilter.length === 0 || this.statusFilter.includes(item.status);
+                return nameMatch && statusMatch;
+            });
+
+            // Aplicar paginación
+            const startIndex = (this.currentPage - 1) * this.rowsPerPage;
+            const endIndex = startIndex + this.rowsPerPage;
+            return filteredData.slice(startIndex, endIndex);
+        },
+        totalFilteredPages() {
+            const filteredData = this.characters.filter(item => {
+                const nameMatch = item.name.toLowerCase().includes(this.textSearch.toLowerCase());
+                const statusMatch = this.statusFilter.length === 0 || this.statusFilter.includes(item.status);
+                return nameMatch && statusMatch;
+            });
+            return Math.ceil(filteredData.length / this.rowsPerPage);
+        }
+    }
+});
+
+app.mount('#app');
